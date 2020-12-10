ae704769813fbc9367284bd2dee428a85e7b804f
dylan.lindgren <dylan.lindgren@servicenow.com>
2020-12-10 17:35:38 +1100

    Committing component source where I removed AC as dependencies and loaded dynamically through script tag

--- components/src/snc-wds-va-adaptive-card/index.js
+++ components/src/snc-wds-va-adaptive-card/index.js
@@ -1,10 +1,6 @@
 import { createCustomElement } from '@servicenow/ui-core';
 import { snabbdom } from '@servicenow/ui-renderer-snabbdom';
 
-import * as ACData from "adaptivecards-templating";
-import * as AdaptiveCards from "adaptivecards";
-import markdownit from "markdown-it";
-
 import styles from './styles.scss';
 import { actionHandlers } from './actionHandlers.js';
 
@@ -12,13 +8,6 @@ import hostConfig from './hostConfig.json';
 import blankCard from '../blankCard.json';
 
 import { CARD_SUBMITTED, URL_OPENED } from '../constants.js';
-
-AdaptiveCards.AdaptiveCard.onProcessMarkdown = function (text, result) {
-	result.outputHtml = markdownit().render(text);
-	result.didProcess = true;
-};
-
-var adaptiveCard = new AdaptiveCards.AdaptiveCard();
 
 const view = (state, { dispatch }) => {
 
@@ -28,24 +17,57 @@ const view = (state, { dispatch }) => {
 		return (<div></div>);
 	}
 
-	adaptiveCard.hostConfig = new AdaptiveCards.HostConfig({...hostConfig});
+	let loaded = 0;
 
-	adaptiveCard.onExecuteAction = function ({_propertyBag, data}) {
-		if (_propertyBag.type === 'Action.OpenUrl') {
-			dispatch(URL_OPENED, _propertyBag);
-		} else if (_propertyBag.type === 'Action.Submit') {
-			dispatch(CARD_SUBMITTED, {
-				propertyBag: _propertyBag,
-				data: data
-			});
+	let acRef = null;
+
+	function areAllLoaded() {
+		loaded++;
+
+		if (loaded === 3) {
+			var adaptiveCard = new AdaptiveCards.AdaptiveCard();
+			adaptiveCard.hostConfig = new AdaptiveCards.HostConfig({ ...hostConfig });
+
+			AdaptiveCards.AdaptiveCard.onProcessMarkdown = function (text, result) {
+				result.outputHtml = markdownit().render(text);
+				result.didProcess = true;
+			}
+
+			adaptiveCard.onExecuteAction = function ({ _propertyBag, data }) {
+				if (_propertyBag.type === 'Action.OpenUrl') {
+					dispatch(URL_OPENED, _propertyBag);
+				} else if (_propertyBag.type === 'Action.Submit') {
+					dispatch(CARD_SUBMITTED, {
+						propertyBag: _propertyBag,
+						data: data
+					});
+				}
+			};
+	
+			var template = new ACData.Template(properties.controlData.cardTemplate);
+			var cardPayload = template.expand({ $root: properties.controlData.cardData });
+			adaptiveCard.parse(cardPayload);
+			acRef.appendChild(adaptiveCard.render());
 		}
-	};
+	}
 
-	var template = new ACData.Template(properties.controlData.cardTemplate);
-	var cardPayload = template.expand({ $root: properties.controlData.cardData });
-	adaptiveCard.parse(cardPayload);
-	const renderedCard = adaptiveCard.render();
-	return <div className="sncWdsVaAdaptiveCards" ref={(n) => { n && n.appendChild(renderedCard) }}></div>;
+	const cardScriptEl = document.createElement("script");
+	cardScriptEl.onload = areAllLoaded;
+	cardScriptEl.src = "https://unpkg.com/adaptivecards/dist/adaptivecards.js";
+
+	const templateScriptEl = document.createElement("script");
+	templateScriptEl.onload = areAllLoaded;
+	templateScriptEl.src = "https://unpkg.com/adaptivecards-templating/dist/adaptivecards-templating.min.js";
+
+	const markdownItEl = document.createElement("script");
+	markdownItEl.onload = areAllLoaded;
+	markdownItEl.src = "https://cdnjs.cloudflare.com/ajax/libs/markdown-it/12.0.3/markdown-it.min.js";
+
+	return (
+		<div ref={(n) => { n && n.appendChild(cardScriptEl) && n.appendChild(templateScriptEl) && n.appendChild(markdownItEl) }}>
+			<div className="sncWdsVaAdaptiveCards" ref={element => acRef = element}></div>
+		</div>
+	)
 };
 
 createCustomElement('snc-wds-va-adaptive-card', {
