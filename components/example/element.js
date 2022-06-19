33b21c34d054296033b92c898068dd12e802211a
dylan.lindgren <dylan.lindgren@servicenow.com>
2022-06-19 13:12:28 +0900

    Renamed folder, removed @servicenow/sdk-ci usage, and updated dependencies for compatibility with San Diego

--- components/example/element.js
+++ components/example/element.js
@@ -1,8 +1,30 @@
-import '../src/snc-wds-va-adaptive-card';
-import '@servicenow/sdk-ci';
-import sampleProps from './adaptiveCards/simple.json';
+import { createCustomElement } from '@servicenow/ui-core';
+import snabbdom from '@servicenow/ui-renderer-snabbdom';
+import flight from './adaptiveCards/flight.json'
+import form from './adaptiveCards/form.json'
+import simple from './adaptiveCards/simple.json'
+import task from './adaptiveCards/task.json'
 
-const el = document.createElement('tool-ci-custom-control-tester');
-el.componentTagName="snc-wds-va-adaptive-card";
-el.initialExampleData=sampleProps;
+import '../src/index.js';
+
+const view = (state, { updateState }) => {
+    return (
+        <div>
+            <snc-wds-va-adaptive-card control-data={flight} />
+            <snc-wds-va-adaptive-card control-data={form} />
+            <snc-wds-va-adaptive-card control-data={simple} />
+            <snc-wds-va-adaptive-card control-data={task} />
+        </div>
+    );
+};
+
+createCustomElement('snc-wds-va-adaptive-card-examples', {
+    initialState: {},
+    renderer: { type: snabbdom },
+    view
+});
+
+const el = document.createElement('main');
 document.body.appendChild(el);
+
+el.innerHTML = '<snc-wds-va-adaptive-card-examples/>';
